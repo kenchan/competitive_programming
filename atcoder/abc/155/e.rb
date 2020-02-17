fbbef84422456e1a86add6df242b5911c4c5c7a0
Takahashi Kenichi <kenichi.taka@gmail.com>
2020-02-17 23:05:49 +0900

    AC!
    愚直なDPだとTLEだったので、似たような解放の人のACをみて条件漏れを見つけた

--- atcoder/abc/155/e.rb
+++ atcoder/abc/155/e.rb
@@ -2,13 +2,15 @@ n = gets.chomp.reverse.each_char.map(&:to_i)
 
 count = 0
 
-n.each_with_index do |num, i|
-  if 10 ** 100 < i
-    count += num
-    next
-  end
+0.upto(n.size) do |i|
+  num = n[i]
+  break unless num
+
   if num == 10
     n[i + 1] = (n[i + 1] || 0) + 1
+  elsif num == 5 && n[i + 1] && n[i + 1] >= 5
+    count += num
+    n[i + 1] += 1
   elsif 5 < num
     n[i + 1] = (n[i + 1] || 0) + 1
     count += 10 - num
