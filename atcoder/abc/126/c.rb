bf1c240396839ab390dec46961069b1d45d3181d
Kenichi Takahashi <kenichi.taka@gmail.com>
2020-10-31 00:33:00 +0900

    AC

--- atcoder/abc/126/c.rb
+++ atcoder/abc/126/c.rb
@@ -1,1 +1,19 @@
-n, k = STDIN.gets.split.map(&:to_i)
+N, K = STDIN.gets.split.map(&:to_i)
+
+def double_count(n , k)
+  if k <= n
+    return 0
+  end
+
+  count = 1
+  while n * 2 < k
+    n *= 2
+    count += 1
+  end
+
+  count
+end
+
+puts 1.upto(N).inject(0) {|acc, n|
+  acc + (1.0 / N) * ((1.0 / 2) ** double_count(n, K))
+}
