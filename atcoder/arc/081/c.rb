1a31f0b33d82779a6883c189c78ea487ce6eed56
Kenichi Takahashi <kenichi.taka@gmail.com>
2020-11-26 23:57:32 +0900

    AC

---
+++ atcoder/arc/081/c.rb
@@ -0,0 +1,17 @@
+N = gets.to_i
+A = gets.split.map(&:to_i)
+
+aa = A.tally.select {|k, v| v >= 2 }
+
+if aa.keys.size < 2
+  puts 0
+  exit
+end
+
+ab = aa.sort_by {|k,v| k * -1 }
+
+if ab[0][1] >= 4
+  puts ab[0][0] ** 2
+else
+  puts ab[0][0] * ab[0][1]
+end
