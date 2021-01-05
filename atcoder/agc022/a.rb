df171b7ae8b287b74f719aabf033b849cbcc8dfa
Kenichi Takahashi <kenichi.taka@gmail.com>
2021-01-05 22:45:22 +0900

    AC

--- atcoder/agc022/a.rb
+++ atcoder/agc022/a.rb
@@ -4,7 +4,7 @@ if S.size == 26
   25.downto(1) do |i|
     unless S[i - 1] > S[i]
       print S[0, (i - 1)].join
-      print S[i..-1].sort.first
+      print S[i..-1].sort.find {|c| S[i - 1] < c }
       print "\n"
       exit;
     end
