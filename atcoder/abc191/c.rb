c1eac0c5f8ef86e05526ec633c0a74f0d090f8db
Kenichi Takahashi <kenichi.taka@gmail.com>
2021-02-07 10:23:11 +0900

    ABC191 C
    ４マスずつみて角になる部分を探していく

--- atcoder/abc191/c.rb
+++ atcoder/abc191/c.rb
@@ -1,26 +1,16 @@
 H, W = gets.split.map(&:to_i)
 S = H.times.map { gets.chomp.split('') }
 
-lr = S.map {|s|
-  l = r = nil
-  s.each_with_index {|c, i|
-    if c == '#'
-      r = i
-      l = i unless l
+ans = 0
+
+(H - 1).times do |h|
+  (W - 1).times do |w|
+    sharp_count = [S[h][w], S[h][w + 1], S[h + 1][w], S[h + 1][w + 1]].count {|c| c == '#' }
+
+    if sharp_count == 1 || sharp_count == 3
+      ans += 1
     end
-  }
-
-  [l, r]
-}
-
-puts lr.each_cons(2).map {|(al, ar), (bl, br)|
-  if ar == nil || br == nil
-    2
-  elsif al == bl && ar == br
-    0
-  elsif al != bl && ar != br
-    4
-  else
-    2
   end
-}.sum
+end
+
+puts ans
