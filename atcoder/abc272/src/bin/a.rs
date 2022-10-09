fcaaf2131091853f3432e30ccacd2f4b0268cfe0
Kenichi Takahashi <kenichi.taka@gmail.com>
2022-10-09 23:49:32 +0900

    hello rust

--- atcoder/abc272/src/bin/a.rs
+++ atcoder/abc272/src/bin/a.rs
@@ -1,3 +1,12 @@
 fn main() {
-    todo!();
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    let _n:i32 = s.trim().parse().unwrap();
+
+    let mut s2 = String::new();
+    std::io::stdin().read_line(&mut s2).unwrap();
+    let v:Vec<u32> = s2.split_whitespace().map(|c| c.parse().unwrap()).collect();
+    let sum:u32 = v.iter().sum();
+
+    println!("{}", sum);
 }
