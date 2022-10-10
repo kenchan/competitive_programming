227b1371ca58c7291b77d3ad56dcf2e27801fbe3
Kenichi Takahashi <kenichi.taka@gmail.com>
2022-10-10 23:09:18 +0900

    proconio::inputをつかって書きなおした。
    sumは型を指定しないとエラーになるがなぜかはまだわかっていない。

--- atcoder/abc272/src/bin/a.rs
+++ atcoder/abc272/src/bin/a.rs
@@ -1,12 +1,9 @@
+use proconio::input;
+
 fn main() {
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).unwrap();
-    let _n:i32 = s.trim().parse().unwrap();
-
-    let mut s2 = String::new();
-    std::io::stdin().read_line(&mut s2).unwrap();
-    let v:Vec<u32> = s2.split_whitespace().map(|c| c.parse().unwrap()).collect();
-    let sum:u32 = v.iter().sum();
-
-    println!("{}", sum);
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+    println!("{}", a.iter().sum::<usize>());
 }
