a3986dd07efa7d358362e3c9ef9601d714b58876
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-01-02 19:40:15 +0900

    ABC 280 C

--- atcoder/abc280/src/bin/c.rs
+++ atcoder/abc280/src/bin/c.rs
@@ -1,3 +1,17 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    for i in 0..s.len() {
+        if s[i] != t[i] {
+            println!("{}", i + 1);
+            return
+        }
+    }
+
+    println!("{}", t.len())
 }
