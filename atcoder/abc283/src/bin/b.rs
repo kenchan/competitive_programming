d21d01f4b2b23d7eb89ec03880eacd62cf4686f3
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-01-09 00:06:30 +0900

    ABC 283 B

--- atcoder/abc283/src/bin/b.rs
+++ atcoder/abc283/src/bin/b.rs
@@ -1,4 +1,33 @@
 use proconio::input;
 
 fn main() {
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        q: usize,
+    }
+
+    for _ in 0..q {
+        input! {
+            q1: usize,
+        }
+
+        match q1 {
+            1 => {
+                input! {
+                    k: usize,
+                    x: usize,
+                }
+                let n = a.get_mut(k - 1).unwrap();
+                *n = x;
+            },
+            2 => {
+                input! {
+                    k: usize
+                }
+                println!("{}", a.get(k - 1).unwrap())
+            }
+            _ => {}
+        }
+    }
 }
