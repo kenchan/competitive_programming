0f5a6d3e9a44595632130be73ae28904c2437f8a
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-01-10 23:31:04 +0900

    ABC 284 D
    i * i * i < n まで探索すれば答えがでるので、単純なループでも間に合う

--- atcoder/abc284/src/bin/d.rs
+++ atcoder/abc284/src/bin/d.rs
@@ -1,3 +1,23 @@
+use num_integer::Roots;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        t: usize,
+        tests: [usize; t],
+    }
+    for n in tests {
+        for i in 2..=n {
+            if n % i != 0 {
+                continue;
+            }
+
+            if n % (i * i) == 0 {
+                println!("{} {}", i, n / i / i);
+            } else {
+                println!("{} {}", (n / i).sqrt(), i);
+            }
+            break;
+        }
+    }
 }
