58e88bacaf3fd2c6b4616cb151b0993c3c3bfc09
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-01-14 00:28:15 +0900

    ABC 282 B
    全探索

--- atcoder/abc282/src/bin/b.rs
+++ atcoder/abc282/src/bin/b.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        _m: usize,
+        s: [String; n],
+    }
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let str1 = &s[i];
+            let str2 = &s[j];
+
+            if str1
+                .chars()
+                .zip(str2.chars())
+                .all(|(c1, c2)| c1 == 'o' || c2 == 'o')
+            {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
