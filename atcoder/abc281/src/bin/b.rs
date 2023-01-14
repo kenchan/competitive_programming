d7513b064c8459a4306ad9927a3b4e3cf94abf6e
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-01-14 23:41:16 +0900

    ABC 281 B

--- atcoder/abc281/src/bin/b.rs
+++ atcoder/abc281/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+use regex::Regex;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let re = Regex::new("^[A-Z][1-9][0-9]{5}[A-Z]$").unwrap();
+
+    if re.is_match(&s) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
