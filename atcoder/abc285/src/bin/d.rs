0e6f5adbf0f70a272d10a8447c40859a5bdd4616
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-01-16 23:13:09 +0900

    ABC 285 D
    DFSっぽくといてみたがコードに無駄がありすぎ

--- atcoder/abc285/src/bin/d.rs
+++ atcoder/abc285/src/bin/d.rs
@@ -9,21 +9,34 @@ fn main() {
     }
 
     let m:HashMap<_, _> = st.into_iter().collect();
+    let mut ok_list = HashSet::new();
 
-    for s in m.keys() {
+    for mut s in m.keys() {
+        if ok_list.contains(s) {
+            continue;
+        }
+
         let mut visits = HashSet::new();
         visits.insert(s.clone());
+        ok_list.insert(s.clone());
 
         loop {
-            let v = m.get(s);
-
             match m.get(s) {
                 Some(v) => {
-                    
+                    if visits.contains(v) {
+                        println!("No");
+                        return;
+                    } else {
+                        visits.insert(v.clone());
+                        ok_list.insert(v.clone());
+                        s = v;
+                    }
                 }
                 None => { break; }
             }
         }
+
     }
 
+    println!("Yes");
 }
