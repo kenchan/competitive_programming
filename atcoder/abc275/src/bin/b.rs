838422850ba6a273cb0432021e18dfe248db7b67
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-01-23 00:04:36 +0900

    ABC 275 B
    x < y になることがあり、その場合は割った数を足す必要がある

--- atcoder/abc275/src/bin/b.rs
+++ atcoder/abc275/src/bin/b.rs
@@ -1,4 +1,28 @@
 use proconio::input;
 
 fn main() {
+    input! {
+        a: isize,
+        b: isize,
+        c: isize,
+        d: isize,
+        e: isize,
+        f: isize,
+    }
+
+    const D: isize = 998244353;
+
+    let mut x = ((a % D) * (b % D)) % D;
+    x = (x * (c % D)) % D;
+    let mut y = ((d % D) * (e % D)) % D;
+    y = (y * (f % D)) % D;
+
+    let ans = x - y;
+
+    if ans < 0 {
+        println!("{}", ans + D);
+    } else {
+        println!("{}", ans);
+    }
+
 }
