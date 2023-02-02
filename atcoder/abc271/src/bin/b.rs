dd62279b28ed5abfffbe7677daec0ca17eeafdec
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-02-02 23:23:58 +0900

    ABC 271 B

--- atcoder/abc271/src/bin/b.rs
+++ atcoder/abc271/src/bin/b.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut aa: Vec<Vec<usize>> = vec![];
+
+    for _ in 0..n {
+        input! {
+            l: usize,
+            a: [usize; l],
+        };
+
+        aa.push(a);
+    }
+
+    for _ in 0..q {
+        input! {
+            s: usize,
+            t: usize,
+        }
+
+        println!("{}", aa[s - 1][t - 1]);
+    }
 }
