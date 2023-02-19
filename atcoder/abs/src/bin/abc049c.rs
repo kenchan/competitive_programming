640f0d5466a76c799b47528a72489b9365d6a977
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-02-19 23:26:45 +0900

    ABS

--- atcoder/abs/src/bin/abc049c.rs
+++ atcoder/abs/src/bin/abc049c.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+use regex::Regex;
+
 fn main() {
-    todo!();
+    input! {
+        mut s: String,
+    }
+
+    let re = Regex::new(r"(dream|dreamer|erase|eraser)$").unwrap();
+
+    loop {
+        if s.len() == 0 {
+            println!("YES");
+            break;
+        }
+
+        let m = re.replace(&s, "");
+
+        if m.len() == s.len() {
+            println!("NO");
+            break
+        }
+
+        s = m.to_string();
+    }
+
 }
--- atcoder/abs/src/bin/abc085c.rs
+++ atcoder/abs/src/bin/abc085c.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        y: usize,
+    }
+
+    for i in 0..=n {
+        for j in 0..=(n - i) {
+            if i * 10000 + j * 5000 + (n - i - j) * 1000 == y {
+                println!("{} {} {}", i, j, n - i - j);
+                return;
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
 }
