c4a8f5e58793854cd2ec1f0c1bb13ae6c67c2c88
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-06-02 23:47:51 +0900

    ABC 302 C

--- atcoder/abc302/C.rb
+++ atcoder/abc302/C.rb
@@ -1,7 +1,25 @@
 # https://atcoder.jp/contests/abc302/tasks/abc302_c
 
-
 N, M = gets.split.map(&:to_i)
-Ss = Array.new(N) { gets.chomp }
+ss = []
+N.times do
+  ss << gets.chomp.split('')
+end
 
-puts cond ? 'Yes' : 'No'
+ss.permutation(N) do |ps|
+  ans = ps.each_cons(2).all? do |pair|
+    d = 0
+    M.times do |i|
+      d += 1 if pair[0][i] != pair[1][i]
+    end
+
+    d == 1
+  end
+
+  if ans
+    puts "Yes"
+    exit
+  end
+end
+
+puts "No"
