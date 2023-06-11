7f7e9d64ef8d39f649b4a5f30d298640c61ca306
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-06-11 21:36:58 +0900

    ABC049 D
    Union-Findで根にそこから繋がっているノードを保持させてみたがTLE

--- atcoder/abc049/D.rb
+++ atcoder/abc049/D.rb
@@ -1,16 +1,64 @@
 # https://atcoder.jp/contests/abc049/tasks/arc065_b
 
+require 'set'
+
+class UnionFindTree
+  def initialize(size)
+    @parents = Array.new(size, -1)
+    @sizes = Array.new(size, 1)
+    @children = size.times.map {|i| Set.new([i]) }
+  end
+
+  def root(x)
+    if @parents[x] < 0
+      x
+    else
+      @parents[x] = root(@parents[x])
+    end
+  end
+
+  def unite(x, y)
+    root_x = root(x)
+    root_y = root(y)
+
+    return if root_x == root_y
+
+    if root_y < root_x
+      root_x, root_y = root_y, root_x
+    end
+
+    @parents[root_y] = root_x
+    @sizes[root_x] += @sizes[root_y]
+    @children[root_x] += @children[root_y]
+  end
+
+  def size(x)
+    @sizes[root(x)]
+  end
+
+  def children(x)
+    @children[root(x)]
+  end
+end
 
 N, K, L = gets.split.map(&:to_i)
-ps = Array.new(K)
-qs = Array.new(K)
+
+pq = UnionFindTree.new(N)
 K.times do |i|
-  ps[i], qs[i] = gets.split.map(&:to_i)
+  pq.unite(*gets.split.map {|i| i.to_i - 1 })
 end
-rs = Array.new(L)
-ss = Array.new(L)
+rs = UnionFindTree.new(N)
 L.times do |i|
-  rs[i], ss[i] = gets.split.map(&:to_i)
+  rs.unite(*gets.split.map {|i| i.to_i - 1 })
 end
 
-puts ans
+ans = []
+
+N.times do |i|
+  pq_c = pq.children(i)
+  rs_c = rs.children(i)
+
+  ans << (pq_c & rs_c).size
+end
+
+puts ans.join(" ")
