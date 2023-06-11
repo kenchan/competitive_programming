1aa2172c9f6355120c2241dabaaf938fa5e93f81
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-06-11 16:31:18 +0900

    ABC120 D
    繋ぐ際に、その橋によってどれだけルートが減るかを計算して、それを引くようにすると間にあった

--- atcoder/abc120/D.rb
+++ atcoder/abc120/D.rb
@@ -1,7 +1,5 @@
 # https://atcoder.jp/contests/abc120/tasks/abc120_d
 
-require 'set'
-
 class UnionFindTree
   def initialize(size)
     @parents = Array.new(size, -1)
@@ -35,41 +33,29 @@ class UnionFindTree
   end
 end
 
-@cache = {}
-
-def factorial(n)
-  (1..n).inject(1, &:*)
-end
-
-
 N, M = gets.split.map(&:to_i)
 As = Array.new(M)
 Bs = Array.new(M)
 M.times do |i|
   As[i], Bs[i] = gets.split.map(&:to_i)
+  As[i] -= 1
+  Bs[i] -= 1
 end
 
-ans = [factorial(N) / factorial(N - 2) / 2]
+ans = [N * (N - 1) / 2]
 
 uf = UnionFindTree.new(N)
 
 M.times do |i|
-  uf.unite(As[M - i - 1] - 1, Bs[M - i - 1] - 1)
-  roots = Set.new
-
-  N.times do |j|
-    roots << uf.root(j)
+  a, b = As[M - i - 1], Bs[M - i - 1]
+  root_a = uf.root(a)
+  root_b = uf.root(b)
+  if root_a == root_b
+    ans << ans.last
+  else
+    ans << ans.last - uf.size(root_a) * uf.size(root_b)
+    uf.unite(a, b)
   end
-
-  a = roots.inject(0) do |acc, root|
-    if uf.size(root) > 1
-      acc + factorial(uf.size(root)) / factorial(uf.size(root) - 2) / 2
-    else
-      acc
-    end
-  end
-
-  ans << ans[0] - a
 end
 
 puts ans.reverse[1..-1].join("\n")
