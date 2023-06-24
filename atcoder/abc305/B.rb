c37e2c4531b076c1378e13f35c30a7678548d92e
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-06-24 23:50:19 +0900

    ABC 305 B

--- atcoder/abc305/B.rb
+++ atcoder/abc305/B.rb
@@ -1,6 +1,7 @@
 # https://atcoder.jp/contests/abc305/tasks/abc305_b
 
-
 p, q = gets.chomp.split
 
-puts ans
+x = %w(A X X B C X X X D E X X X X F X X X X X X X X G)
+
+puts (x.index(p) - x.index(q)).abs
