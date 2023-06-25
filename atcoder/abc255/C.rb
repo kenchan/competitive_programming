3cd9f55c259d716bf75dfa42f9c5fecc6bfbe9bc
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-06-25 17:18:42 +0900

    ABC 255 C
    すべてを正の数にして、xの位置で判定する

--- atcoder/abc255/C.rb
+++ atcoder/abc255/C.rb
@@ -1,6 +1,29 @@
 # https://atcoder.jp/contests/abc255/tasks/abc255_c
 
+x, a, d, n = gets.split.map(&:to_i)
 
-X, A, D, N = gets.split.map(&:to_i)
+n -= 1
 
-puts ans
+if d == 0
+  puts (a - x).abs
+  exit
+elsif d < 0
+  a = a + d * n
+  d = d.abs
+end
+
+if a < 0 || x < 0
+  min = [a, x].min.abs
+  a += min
+  x += min
+end
+
+if x <= a
+  puts a - x
+elsif a + d * n <= x
+  puts x - (a + d * n)
+else
+  i = ((x - a) / d)
+  puts [x - (a + (d * i)), (a + (d * (i + 1))) - x].min
+end
+
