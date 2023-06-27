e9fd4ec4766439df470e2e2d45549fb51c11973a
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-06-27 20:33:53 +0900

    ABC023 B
    手順が決まっていて長さも100なので作っていけばよい。
    'b'だけのとき(=0回)のものを考慮するとループの最初で判定する必要がある。

--- atcoder/abc023/B.rb
+++ atcoder/abc023/B.rb
@@ -1,7 +1,25 @@
 # https://atcoder.jp/contests/abc023/tasks/abc023_b
 
-
 N = gets.to_i
 S = gets.chomp
 
-puts ans
+str = "b"
+
+100.times do |i|
+  if S == str
+    puts i
+    exit
+  elsif S.size < str.size
+    puts '-1'
+    exit
+  end
+
+  case i % 3
+  when 0
+    str = 'a' + str + 'c'
+  when 1
+    str = 'c' + str + 'a'
+  when 2
+    str = 'b' + str + 'b'
+  end
+end
