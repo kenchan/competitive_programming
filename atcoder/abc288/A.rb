02aa5158fc0fbf85c2761a6e9d3ac0d37c09a348
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-07-12 23:13:34 +0900

    ABC288 A

--- atcoder/abc288/A.rb
+++ atcoder/abc288/A.rb
@@ -1,11 +1,7 @@
 # https://atcoder.jp/contests/abc288/tasks/abc288_a
 
-
 N = gets.to_i
-As = Array.new(N)
-Bs = Array.new(N)
-N.times do |i|
-  As[i], Bs[i] = gets.split.map(&:to_i)
-end
 
-puts ans
+N.times do |i|
+  puts gets.split.map(&:to_i).sum
+end
