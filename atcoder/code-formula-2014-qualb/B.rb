7d351f58dd40f24a0f46d7aff9c317284fa55ccc
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-07-13 20:24:05 +0900

    code-formula-2014-qualb B

--- atcoder/code-formula-2014-qualb/B.rb
+++ atcoder/code-formula-2014-qualb/B.rb
@@ -1,6 +1,11 @@
 # https://atcoder.jp/contests/code-formula-2014-qualb/tasks/code_formula_2014_qualB_b
 
-
 N = gets.chomp
 
-puts ans
+ans = [0, 0]
+
+N.chars.reverse.each.with_index do |c, i|
+  ans[(i + 1) % 2] += c.to_i
+end
+
+puts ans.join(' ')
