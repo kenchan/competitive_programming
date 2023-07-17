5045ff37cbb41916eb46678eb8c8782e96fb544d
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-07-17 22:17:23 +0900

    ABC276 D
    2と3で割っていき、残った数が一致していなければ-1、それぞれの最小値との差を足していくと答えになる。

--- atcoder/abc276/D.rb
+++ atcoder/abc276/D.rb
@@ -1,7 +1,37 @@
 # https://atcoder.jp/contests/abc276/tasks/abc276_d
 
 
+def div(n, i)
+  c = 0
+  while n % i == 0
+    n /= i
+    c += 1
+  end
+
+  return [n, c]
+end
+
 N = gets.to_i
 as = gets.split.map(&:to_i)
+
+ds = as.map {|a|
+  n, c3 = *div(a, 3)
+  m, c2 = *div(n, 2)
+  [m, c2, c3]
+}
+
+mod = ds[0][0]
+min_c2 = ds.min {|a, b| a[1] <=> b[1] }[1]
+min_c3 = ds.min {|a, b| a[2] <=> b[2] }[2]
+ans = 0
+
+ds.each do |n, c2, c3|
+  if mod != n
+    puts "-1"
+    exit
+  end
+
+  ans += (c2 - min_c2) + (c3 - min_c3)
+end
 
 puts ans
