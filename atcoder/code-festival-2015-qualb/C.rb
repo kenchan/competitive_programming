cd1ea777ae3e52f97442d021a7cfe51e342dfc3d
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-07-18 23:42:48 +0900

    code-festival-2015-qualb C
    ソートしてから両方のindexを保持しつつループをまわす。最後までループがまわったらYES、途中で部屋がみつなからなかったらNOを出力すればよい。

--- atcoder/code-festival-2015-qualb/C.rb
+++ atcoder/code-festival-2015-qualb/C.rb
@@ -5,4 +5,31 @@ N, M = gets.split.map(&:to_i)
 As = gets.split.map(&:to_i)
 Bs = gets.split.map(&:to_i)
 
-puts cond ? 'YES' : 'NO'
+if N < M
+  puts 'NO'
+  exit
+end
+
+As.sort!
+Bs.sort!
+
+ai = 0
+bi = 0
+
+bi.upto(M - 1) do |i|
+  prev = ai
+  ai.upto(N - 1) do |j|
+    if Bs[bi] <= As[j]
+      ai = j + 1
+      bi = i + 1
+      break
+    end
+  end
+
+  if prev == ai
+    puts "NO"
+    exit
+  end
+end
+
+puts "YES"
