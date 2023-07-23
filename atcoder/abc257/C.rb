847d88247b52a792036e15befc62c05635ee7cdb
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-07-23 16:05:59 +0900

    ABC257 C
    体重毎の人数を数えておいて、小さい方から区切りを動かしていくと人数がどう変化するかを見ていけばよい

--- atcoder/abc257/C.rb
+++ atcoder/abc257/C.rb
@@ -4,22 +4,29 @@ N = gets.to_i
 S = gets.chomp.chars.map(&:to_i)
 Ws = gets.split.map(&:to_i)
 
-Ws << Ws.max + 1
-
-dp = Array.new(N + 1) { Array.new(Ws.size, 0) }
-
-N.times do |i|
-  w = Ws[i]
-  s = S[i]
-
-  Ws.each_with_index do |w2, je|
-    correct = if s == 0
-      w < w2 ? 1 : 0
-    else
-      w2 <= w ? 1 : 0
-    end
-    dp[i + 1][je] = dp[i][je] + correct
+m = N.times.inject({}) {|acc, i|
+  acc[Ws[i]] ||= [0, 0]
+  if S[i] == 0
+    acc[Ws[i]][0] += 1
+  else
+    acc[Ws[i]][1] += 1
   end
+  acc
+}
+
+init = S.count {|s| s == 1 }
+
+ws = m.keys.sort
+
+ans = [init]
+
+ws.each do |w|
+  count = ans.last
+
+  child, adult = *m[w]
+
+  ans << count + child - adult
 end
 
-puts dp.last.max
+puts ans.max
+
