3d4070ebd09032c65830b82cfd39e92ad32dd9a7
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-07-30 23:05:50 +0900

    ABC269 B

--- atcoder/abc269/B.rb
+++ atcoder/abc269/B.rb
@@ -1,6 +1,16 @@
 # https://atcoder.jp/contests/abc269/tasks/abc269_b
 
-
 Ss = Array.new(10) { gets.chomp }
 
-puts ans
+ans = []
+
+10.times do |i|
+  10.times do |j|
+    if Ss[i][j] == "#"
+      ans << [i + 1, j + 1]
+    end
+  end
+end
+
+puts "#{ans.map(&:first).min} #{ans.map(&:first).max}"
+puts "#{ans.map(&:last).min} #{ans.map(&:last).max}"
