96dee67a5864541e9f0a1f78dfef5de9d9743583
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-08-16 23:34:05 +0900

    ABC227 B

--- atcoder/abc227/B.rb
+++ atcoder/abc227/B.rb
@@ -1,7 +1,19 @@
 # https://atcoder.jp/contests/abc227/tasks/abc227_b
 
+def solve(s)
+  1.upto(s) {|a|
+    1.upto(s) {|b|
+      if 4 * a * b + 3 * a + 3 * b == s
+        return true
+      elsif 4 * a * b + 3 * a + 3 * b > s
+        break
+      end
+    }
+  }
+  return false
+end
 
 N = gets.to_i
 Ss = gets.split.map(&:to_i)
 
-puts ans
+puts Ss.reject {|s| solve(s) }.count
