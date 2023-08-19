93196bd1f5b9cfbbe47763c6ae1b20a39ae0e9f1
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-08-19 22:39:13 +0900

    ABC232 C
    Nが小さいので全探索

--- atcoder/abc232/C.rb
+++ atcoder/abc232/C.rb
@@ -1,16 +1,31 @@
 # https://atcoder.jp/contests/abc232/tasks/abc232_c
 
-
 N, M = gets.split.map(&:to_i)
-As = Array.new(M)
-Bs = Array.new(M)
-M.times do |i|
-  As[i], Bs[i] = gets.split.map(&:to_i)
-end
-Cs = Array.new(M)
-Ds = Array.new(M)
-M.times do |i|
-  Cs[i], Ds[i] = gets.split.map(&:to_i)
+
+AB = Array.new(N) { Array.new(N, 0) }
+M.times do
+  a, b = gets.split.map {|i| i.to_i - 1}
+  AB[a][b] = 1
+  AB[b][a] = 1
 end
 
-puts cond ? 'Yes' : 'No'
+CD = Array.new(N) { Array.new(N, 0) }
+M.times do
+  c, d = gets.split.map {|i| i.to_i - 1}
+  CD[c][d] = 1
+  CD[d][c] = 1
+end
+
+
+ans = N.times.to_a.permutation(N).find {|ns|
+  N.times.all? {|i|
+    N.times.all? {|j|
+      AB[i][j] == CD[ns[i]][ns[j]]
+    }
+  }
+}
+
+puts ans ? 'Yes' : 'No'
+
+
+
