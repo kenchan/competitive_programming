afadc853eb843c43c4441c4962d8f24b552f8e3c
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-08-21 19:42:48 +0900

    code-festibal-2014-final C
    Aがmaxでも答は10_000なので全探索できる

--- atcoder/code-festival-2014-final/C.rb
+++ atcoder/code-festival-2014-final/C.rb
@@ -1,6 +1,17 @@
 # https://atcoder.jp/contests/code-festival-2014-final/tasks/code_festival_final_c
 
-
 A = gets.to_i
 
-puts ans
+10.upto(10000) do |ans|
+  num = 0
+  ans.to_s.chars.reverse.each_with_index do |c, i|
+    num += c.to_i * (ans ** i.to_i)
+  end
+
+  if num == A
+    puts ans
+    exit
+  end
+end
+
+puts -1
