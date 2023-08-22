e7f1393a05f8516d4cb09bf8796d6b3e012256d3
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-08-22 21:27:47 +0900

    ABC272 B

--- atcoder/abc272/B.rb
+++ atcoder/abc272/B.rb
@@ -1,11 +1,27 @@
 # https://atcoder.jp/contests/abc272/tasks/abc272_b
 
+require 'set'
 
 N, M = gets.split.map(&:to_i)
-ks = Array.new(M)
-xss = Array.new(M)
-M.times do |i|
-  ks[i], *xss[i] = gets.split.map(&:to_i)
+
+xx = Array.new(N) { Array.new(N, false) }
+
+M.times do
+  kx = gets.split.map(&:to_i)
+  kx[1..-1].combination(2).each do |x1, x2|
+    xx[x1 - 1][x2 - 1] = true
+    xx[x2 - 1][x1 - 1] = true
+  end
 end
 
-puts cond ? 'Yes' : 'No'
+N.times do |i|
+  N.times do |j|
+    next if i == j
+    unless xx[i][j]
+      puts 'No'
+      exit
+    end
+  end
+end
+
+puts 'Yes'
